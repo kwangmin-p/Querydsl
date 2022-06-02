1f0a46cef05bb4dfcb49ea66d7ccadcff08e9979
kwangmin-park <96bgm@korea.ac.kr>
2022-06-02 15:42:18 +0900

    기본 모델 Entity 생성

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,47 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+//ToString에서 team은 제외시키는것이 좋음. 무한루프 발생 가능성.
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="team_id")
+    private Team team;
+//    여기가 연관관계의 주인
+
+    public Member(String username){
+        this(username, 0);
+    }
+
+    public Member(String username, int age){
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team tem){
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
