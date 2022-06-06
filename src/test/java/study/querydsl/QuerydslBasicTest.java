cf85cca1c1916f6564966c138a9c91e15953cdb9
gmpark <huiae07@gmail.com>
2022-06-07 06:32:07 +0900

    조인 - 기본조인 연습

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -232,6 +232,44 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
 
     }
+
+    /*
+    * 팀 A에 소속된 모든 회원원
+   * */
+    @Test
+    public void join(){
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team) //member.team과 Qteam.team을 조인 / 기본적으로 join은 inner join
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /*
+    * 연관관계 없는 경우
+    * 세타 조인
+    * 회원의 이름이 팀 이름과 같은 회원 조회
+    * */
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team) //join이 아니라 from절에서 여러 테이블을 가져온뒤 where로 매칭
+                .where(member.username.eq(team.name)) // 이 방식으로는 외부조인(left outer, right outer join이 불가능). -> on 을 사용하여 외부 조인 가능
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
 
 
